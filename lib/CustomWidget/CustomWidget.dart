f2c7296390797011e130f3fcc6eff5d4dbabe751
Vladimir Kozhevnikov <kogeva19898@gmail.com>
2019-05-22 23:14:18 +0300

    add rich text

--- lib/CustomWidget/CustomWidget.dart
+++ lib/CustomWidget/CustomWidget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomWidget extends StatelessWidget
-{
+class CustomWidget extends StatelessWidget {
   String text;
 
   CustomWidget(String text) {
@@ -14,21 +13,15 @@ class CustomWidget extends StatelessWidget
       alignment: Alignment.topRight,
       child: Container(
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            color: Colors.blue
-        ),
+            borderRadius: BorderRadius.circular(15.0), color: Colors.blue),
         margin: EdgeInsets.only(right: 10.0, left: 50, bottom: 5, top: 5),
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-        child: Column(children: <Widget>[
-          Text(text, style: TextStyle(
-              color: Colors.white,
-              fontSize: 15.0
-          ),),
-          Text( '11:24', style: TextStyle(
-              color: Colors.white,
-              fontSize: 14.0
-          ),),
-        ],),
+        child: RichText(
+            text: TextSpan(children: <TextSpan>[
+          TextSpan(text: text, children: <TextSpan>[
+            TextSpan(text: " 11:11", style: TextStyle())
+          ])
+        ])),
       ),
     );
   }
