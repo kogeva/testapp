89f6e2e80f159fe98132f95d9ca25057a9847f11
Vladimir Kozhevnikov <kogeva19898@gmail.com>
2019-05-24 00:57:16 +0300

    custom widget

--- lib/CustomWidget/CustomWidget.dart
+++ lib/CustomWidget/CustomWidget.dart
@@ -2,27 +2,110 @@ import 'package:flutter/material.dart';
 
 class CustomWidget extends StatelessWidget {
   String text;
+  RichText richText;
 
   CustomWidget(String text) {
     this.text = text;
+
+    this.richText = RichText(
+        text: TextSpan(children: <TextSpan>[
+      TextSpan(text: text, children: <TextSpan>[
+        TextSpan(
+          text: " 11:12",
+          style: TextStyle(),
+        )
+      ])
+    ]));
   }
 
   @override
   Widget build(BuildContext context) {
+    this.richText.toStringShallow();
+
+    Container container = Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0), color: Colors.blue),
+      margin: EdgeInsets.only(right: 10.0, left: 50, bottom: 5, top: 5),
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+      child: this.richText,
+    );
+
+    Stack stack = Stack(
+      children: <Widget>[richText],
+    );
+
     return Align(
       alignment: Alignment.topRight,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0), color: Colors.blue),
-        margin: EdgeInsets.only(right: 10.0, left: 50, bottom: 5, top: 5),
-        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-        child: RichText(
-            text: TextSpan(children: <TextSpan>[
-          TextSpan(text: text, children: <TextSpan>[
-            TextSpan(text: " 11:11", style: TextStyle())
-          ])
-        ])),
+      child: container,
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final String msg;
+  final String additionalInfo;
+
+  CustomCard({this.msg = "hello world", this.additionalInfo = ""});
+
+  TextStyle style = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w400,
+      fontSize: 15.5,
+      letterSpacing: 0.3);
+
+  @override
+  Widget build(BuildContext context) {
+    style = Theme.of(context).textTheme.subtitle.merge(style);
+
+    Container card = Container(
+      decoration: new BoxDecoration(
+          borderRadius: new BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+              bottomLeft: Radius.circular(10)),
+          color: Colors.blueGrey[500]),
+      margin: EdgeInsets.only(right: 10.0, left: 50, bottom: 5, top: 5),
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 8, top: 8, right: 8, bottom: 8),
+            child: RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  //real message
+                  TextSpan(
+                    text: msg + "  ",
+                    style: style,
+                  ),
+
+                  //fake additionalInfo as placeholder
+                  TextSpan(
+                      text: additionalInfo,
+                      style:
+                          TextStyle(fontSize: 12.0, color: Color.fromRGBO(255, 255, 255, 0))),
+                ],
+              ),
+            ),
+          ),
+
+          //real additionalInfo
+          Positioned(
+            child: Text(
+              additionalInfo,
+              style: TextStyle(
+                  fontSize: 12.0, color: Color.fromRGBO(255, 255, 255, 1)),
+            ),
+            right: 10.0,
+            bottom: 5.0,
+          )
+        ],
       ),
+    );
+
+    return Align(
+      alignment: Alignment.topRight,
+      child: card,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -37,29 +37,103 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    ListView list = ListView(
+//      scrollDirection: Axis.vertical,
+//      shrinkWrap: true,
+      children: <Widget>[
+        CustomCard(
+          msg:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          additionalInfo: "11:24",
+        ),
+        CustomCard(
+          msg: "hello world",
+          additionalInfo: "12:00",
+        ),
+        CustomCard(
+          msg: "hello world",
+          additionalInfo: "12:00",
+        ),
+        CustomCard(
+          msg: "hello world",
+          additionalInfo: "12:00",
+        ),
+        CustomCard(
+          msg: "hello world",
+          additionalInfo: "12:00",
+        ),
+        CustomCard(
+          msg: "hello world",
+          additionalInfo: "12:00",
+        ),
+        CustomCard(
+          msg: "hello world",
+          additionalInfo: "12:00",
+        ),
+        CustomCard(
+          msg: "hello world",
+          additionalInfo: "12:00",
+        ),
+        CustomCard(
+          msg: "hello world",
+          additionalInfo: "12:00",
+        ),
+        CustomCard(
+          msg: "hello world",
+          additionalInfo: "12:00",
+        ),
+        CustomCard(
+          msg:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          additionalInfo: "11:24",
+        ),
+        CustomCard(
+          msg:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          additionalInfo: "11:24",
+        ),
+        CustomCard(
+          msg:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          additionalInfo: "11:24",
+        ),
+        CustomCard(
+          msg:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          additionalInfo: "11:24",
+        ),
+        CustomCard(
+          msg:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          additionalInfo: "11:24",
+        ),
+        CustomCard(
+          msg:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          additionalInfo: "11:24",
+        ),
+
+      ],
+    );
+
+    Row controlInput = Row(
+      children: <Widget>[
+        TextField()
+      ],
+    );
+
+    Column column = Column(
+      children: <Widget>[
+        list,
+//        new Expanded(child: new TextFormField()),
+      ],
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: ListView(children: <Widget>[
-          Column(
-//              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                CustomWidget('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
-                CustomWidget('Hello world'),
-                CustomWidget('Hello world'),
-                CustomWidget('Hello world'),
-                CustomWidget('Hello world'),
-              ]
-          )
-        ],)
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add)
-      ),
+      body: Center(child: list),
     );
   }
 }
